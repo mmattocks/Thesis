16e6db08284d2854f373f791c02dffce3b9665d8
Michael Mattocks <mike.mattocks@utoronto.ca>
2020-11-16 16:26:42 -0500

    writing commit

--- analyses/cmz/a10periodisation.jl
+++ analyses/cmz/a10periodisation.jl
@@ -6,7 +6,7 @@ a10pth="/bench/PhD/datasets/A10 measurements 2018update.csv"
 e2ph="/bench/PhD/NGS_binaries/BSS/A10/e2ph"
 e3ph="/bench/PhD/NGS_binaries/BSS/A10/e3ph"
 
-paths=[e2ph,e3ph]
+paths=[e3ph]
 
 a10df=DataFrame(CSV.read(a10pth))
 a10df.BSR=[string(row.Block,',',row.Slide,',',row.Row) for row in eachrow(a10df)]
@@ -84,13 +84,12 @@ voldist=fit(LogNormal,measure_dict["SphEst"][1])
 
 ph2_constants=[X, popdist, voldist, vol_const, mc_its, 2]
 ph3_constants=[X, popdist, voldist, vol_const, mc_its, 3]
-constants=ph2_constants,ph3_constants
+constants=[ph3_constants]
 
-cycle_prior=[Uniform(10.,144.), Uniform(0.,6)]
+cycle_prior=[Uniform(10.,144.), LogNormal(log(.9),log(2))]
 end_prior=[Uniform(4.,359.)]
 
-phases=[2,3]
-phase_max=[144.,6.]
+phase_max=[144.,5.]
 phase_min=[10.,0.]
 time_max=359
 time_min=4.
@@ -108,6 +107,7 @@ function compose_priors(phases)
     return prs,bxes
 end
 
+phases=[3]
 priors,boxes=compose_priors(phases)
 
 uds=Vector{Vector{Function}}([[],[liwi_display],[convergence_display]])
@@ -123,9 +123,9 @@ for (pth,prior,constants,box) in zip(paths,priors,constants,boxes)
     converge_ensemble!(e,backup=(true,50),upper_displays=uds, lower_displays=lds, disp_rot_its=100, mc_noise=.3, converge_criterion="compression", converge_factor=1.)
 end
 
-e2=deserialize(e2ph*"/ens")
-e3=deserialize(e3ph*"/ens")
+# e2=deserialize(e2ph*"/ens")
+# e3=deserialize(e3ph*"/ens")
 
-kde2=posterior_kde(e2)
-kde3=posterior_kde(e3)
+# kde2=posterior_kde(e2)
+# kde3=posterior_kde(e3)
 
