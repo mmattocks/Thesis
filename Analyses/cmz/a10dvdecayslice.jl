6cc085a08518248f864739d6721054b1478f4915
Michael Mattocks <mike.mattocks@utoronto.ca>
2021-03-18 02:53:18 -0400

    update

--- Analyses/cmz/a10dvdecayslice.jl
+++ Analyses/cmz/a10dvdecayslice.jl
@@ -79,7 +79,6 @@ uds=Vector{Vector{Function}}([[],[liwi_display],[convergence_display]])
 lds=Vector{Vector{Function}}([[model_obs_display],[ensemble_display],[ensemble_display]])
 
 gmc_settings=GMC_DEFAULTS
-gmc_settings[end]=25000
 gmc_settings[end]=100
 
 for (pth,constants,obs) in zip(paths,constants,obsset)
@@ -96,153 +95,79 @@ for (pth,constants,obs) in zip(paths,constants,obsset)
     converge_ensemble!(e,backup=(true,20),upper_displays=uds, lower_displays=lds, disp_rot_its=50, mc_noise=.3, converge_factor=1e-6)
 end
 
-edm=deserialize(pth*"/ens")
+ed=deserialize(edpth*"/ens")
+ev=deserialize(evpth*"/ens")
+em=deserialize(empth*"/ens")
 
-edmev=measure_evidence(edm)
+edev=measure_evidence(ed)
+evev=measure_evidence(ev)
+emev=measure_evidence(em)
 
-# evidence_ratio=emev-(edev+evev)
+evidence_ratio=emev-(edev+evev)
 
-# println("\\begin{tabular}{|l|l|l|l|l|}")
-# println("\\hline")
-# println("{\\bf Combined D/V model logZ} & {\\bf Separate D/V model logZ} & {\\bf logZR} & {\\bf \$\\sigma\$ Significance}\\\\ \\hline")
-# println("\\textbf{$(round(emev,digits=3))} & $(round(edev+evev,digits=3)) & $(round(evidence_ratio,digits=3)) & $(round(evidence_ratio.val/evidence_ratio.err,digits=3))\\\\ \\hline")
-# println("\\end{tabular}")
+println("\\begin{tabular}{|l|l|l|l|l|}")
+println("\\hline")
+println("{\\bf Combined D/V model logZ} & {\\bf Separate D/V model logZ} & {\\bf logZR} & {\\bf \$\\sigma\$ Significance}\\\\ \\hline")
+println("\\textbf{$(round(emev,digits=3))} & $(round(edev+evev,digits=3)) & $(round(evidence_ratio,digits=3)) & $(round(evidence_ratio.val/evidence_ratio.err,digits=3))\\\\ \\hline")
+println("\\end{tabular}")
 
-# mapd=deserialize(ed.models[findmax([m.log_Li for m in ed.models])[2]].path)
-# mapv=deserialize(ev.models[findmax([m.log_Li for m in ev.models])[2]].path)
-# mapm=deserialize(em.models[findmax([m.log_Li for m in em.models])[2]].path)
+mapd=deserialize(ed.models[findmax([m.log_Li for m in ed.models])[2]].path)
+mapv=deserialize(ev.models[findmax([m.log_Li for m in ev.models])[2]].path)
+mapm=deserialize(em.models[findmax([m.log_Li for m in em.models])[2]].path)
 
-# println("\\begin{tabular}{|l|l|l|l|}")
-# println("\\hline")
-# println("{\\bf Parameter} & {\\bf Total MAP} & {\\bf Dorsal MAP} & {\\bf Ventral MAP}\\ \\hline")
-# println("Phase 1 \$CT\$ (h) & $(round(mapm.θ[1], digits=1)) & $(round(mapd.θ[1], digits=1)) & $(round(mapv.θ[1], digits=1))\\\\ \\hline")
-# println("Phase 1 \$\\epsilon\$ & $(round(mapm.θ[2], digits=2)) & $(round(mapd.θ[2], digits=2)) & $(round(mapv.θ[2], digits=2))\\\\ \\hline")
-# println("Phase 2 \$CT\$ (h) & $(round(mapm.θ[3], digits=1)) & $(round(mapd.θ[3], digits=1)) & $(round(mapv.θ[3], digits=1))\\\\ \\hline")
-# println("Phase 2 \$\\epsilon\$ & $(round(mapm.θ[4], digits=2)) & $(round(mapd.θ[4], digits=2)) & $(round(mapv.θ[4], digits=2))\\\\ \\hline")
-# println("Transition age & $(round(mapm.θ[5], digits=1)) & $(round(mapd.θ[5], digits=1)) & $(round(mapv.θ[5], digits=1))\\\\ \\hline")
-# println("\\end{tabular}")
+println("\\begin{tabular}{|l|l|l|l|}")
+println("\\hline")
+println("{\\bf Parameter} & {\\bf Total MAP} & {\\bf Dorsal MAP} & {\\bf Ventral MAP}\\ \\hline")
+println("Phase 1 \$CT\$ (h) & $(round(mapm.θ[1], digits=1)) & $(round(mapd.θ[1], digits=1)) & $(round(mapv.θ[1], digits=1))\\\\ \\hline")
+println("Phase 1 \$\\epsilon\$ & $(round(mapm.θ[2], digits=2)) & $(round(mapd.θ[2], digits=2)) & $(round(mapv.θ[2], digits=2))\\\\ \\hline")
+println("Phase 2 \$CT\$ (h) & $(round(mapm.θ[3], digits=1)) & $(round(mapd.θ[3], digits=1)) & $(round(mapv.θ[3], digits=1))\\\\ \\hline")
+println("Phase 2 \$\\epsilon\$ & $(round(mapm.θ[4], digits=2)) & $(round(mapd.θ[4], digits=2)) & $(round(mapv.θ[4], digits=2))\\\\ \\hline")
+println("Transition age & $(round(mapm.θ[5], digits=1)) & $(round(mapd.θ[5], digits=1)) & $(round(mapv.θ[5], digits=1))\\\\ \\hline")
+println("\\end{tabular}")
 
-# X=ed.constants[1]
+X=ed.constants[1]
 
-# plotticks=[10*i for i in 1:9]
+plotticks=[10*i for i in 1:9]
 
-# catdobs=vcat([ed.obs[t] for t in 1:length(X)]...)
-# catvobs=vcat([ev.obs[t] for t in 1:length(X)]...)
+catdobs=vcat([ed.obs[t] for t in 1:length(X)]...)
+catvobs=vcat([ev.obs[t] for t in 1:length(X)]...)
 
-# dXs=vcat([[X[n] for i in 1:length(ed.obs[n])] for n in 1:length(X)]...)
-# vXs=vcat([[X[n] for i in 1:length(ev.obs[n])] for n in 1:length(X)]...)
-# dvXs=vcat([[X[n] for i in 1:length(em.obs[n])] for n in 1:length(X)]...)
+dXs=vcat([[X[n] for i in 1:length(ed.obs[n])] for n in 1:length(X)]...)
+vXs=vcat([[X[n] for i in 1:length(ev.obs[n])] for n in 1:length(X)]...)
+dvXs=vcat([[X[n] for i in 1:length(em.obs[1][n])] for n in 1:length(X)]...)
 
-# d_mean=mapd.disp_mat[:,2]
-# d_upper=mapd.disp_mat[:,3].-mapd.disp_mat[:,2]
-# d_lower=mapd.disp_mat[:,2].-mapd.disp_mat[:,1]
+d_mean=mapd.disp_mat[:,2]
+d_upper=mapd.disp_mat[:,3].-mapd.disp_mat[:,2]
+d_lower=mapd.disp_mat[:,2].-mapd.disp_mat[:,1]
 
-# v_mean=mapv.disp_mat[:,2]
-# v_upper=mapv.disp_mat[:,3].-mapv.disp_mat[:,2]
-# v_lower=mapv.disp_mat[:,2].-mapv.disp_mat[:,1]
+v_mean=mapv.disp_mat[:,2]
+v_upper=mapv.disp_mat[:,3].-mapv.disp_mat[:,2]
+v_lower=mapv.disp_mat[:,2].-mapv.disp_mat[:,1]
 
-# md_mean=mapm.slices[1].disp_mat[:,2]
-# md_upper=mapm.slices[1].disp_mat[:,3].-mapm.slices[1].disp_mat[:,2]
-# md_lower=mapm.slices[1].disp_mat[:,2].-mapm.slices[1].disp_mat[:,1]
+md_mean=mapm.slices[1].disp_mat[:,2]
+md_upper=mapm.slices[1].disp_mat[:,3].-mapm.slices[1].disp_mat[:,2]
+md_lower=mapm.slices[1].disp_mat[:,2].-mapm.slices[1].disp_mat[:,1]
 
-# mv_mean=mapm.slices[2].disp_mat[:,2]
-# mv_upper=mapm.slices[2].disp_mat[:,3].-mapm.slices[2].disp_mat[:,2]
-# mv_lower=mapm.slices[2].disp_mat[:,2].-mapm.slices[2].disp_mat[:,1]
+mv_mean=mapm.slices[2].disp_mat[:,2]
+mv_upper=mapm.slices[2].disp_mat[:,3].-mapm.slices[2].disp_mat[:,2]
+mv_lower=mapm.slices[2].disp_mat[:,2].-mapm.slices[2].disp_mat[:,1]
 
-# mapmd_plt=scatter(dXs,catdobs, marker=:cross, color=:black, markersize=3, label="Dorsal CMZ population data", showaxis=:y, xticks=plotticks, xformatter=_->"", ylabel="Population")
-# plot!(mapmd_plt, X, md_mean, ribbon=(md_lower,md_upper), color=:green, label="Combined model D population")
-# annotate!([(8,200,Plots.text("A",18))])
+mapmd_plt=scatter(dXs,catdobs, marker=:cross, color=:black, markersize=3, label="Dorsal CMZ population data", showaxis=:y, xformatter=_->"", ylabel="Population")
+plot!(mapmd_plt, X, md_mean, ribbon=(md_lower,md_upper), color=:green, label="Combined model D population")
+annotate!([(8,200,Plots.text("A",18))])
 
-# mapmv_plt=scatter(vXs,catvobs, marker=:cross, color=:black, markersize=3, label="Ventral CMZ population data", showaxis=:y, ylabel="Population", xticks=plotticks, xformatter=_->"",)
-# plot!(mapmv_plt, X, mv_mean, ribbon=(mv_lower,mv_upper), color=:darkmagenta, label="Combined model V population")
-# annotate!([(8,145,Plots.text("B",18))])
+mapmv_plt=scatter(vXs,catvobs, marker=:cross, color=:black, markersize=3, label="Ventral CMZ population data", showaxis=:y, ylabel="Population", xformatter=_->"",)
+plot!(mapmv_plt, X, mv_mean, ribbon=(mv_lower,mv_upper), color=:darkmagenta, label="Combined model V population")
+annotate!([(8,145,Plots.text("B",18))])
 
-# mapd_plt=scatter(dXs,catdobs, marker=:cross, color=:black, markersize=3, label="Dorsal CMZ population data", xlabel="Age (dpf)", xticks=plotticks, ylabel="Population")
-# plot!(mapd_plt, X, d_mean, ribbon=(d_lower,d_upper), color=:green, label="Separate model D population")
-# annotate!([(8,150,Plots.text("C",18))])
+mapd_plt=scatter(dXs,catdobs, marker=:cross, color=:black, markersize=3, label="Dorsal CMZ population data", xlabel="Age (dpf)", xticks=X, ylabel="Population")
+plot!(mapd_plt, X, d_mean, ribbon=(d_lower,d_upper), color=:green, label="Separate model D population")
+annotate!([(8,150,Plots.text("C",18))])
 
-# mapv_plt=scatter(vXs,catvobs, marker=:cross, color=:black, markersize=3, label="Ventral CMZ population data", xlabel="Age (dpf)", ylabel="Population", xticks=plotticks)
-# plot!(mapv_plt, X, v_mean, ribbon=(v_lower,v_upper), color=:darkmagenta, label="Separate model V population")
-# annotate!([(8,200,Plots.text("D",18))])
+mapv_plt=scatter(vXs,catvobs, marker=:cross, color=:black, markersize=3, label="Ventral CMZ population data", xlabel="Age (dpf)", ylabel="Population", xticks=X)
+plot!(mapv_plt, X, v_mean, ribbon=(v_lower,v_upper), color=:darkmagenta, label="Separate model V population")
+annotate!([(8,250,Plots.text("D",18))])
 
-# combined_map=Plots.plot(mapmd_plt,mapmv_plt,mapd_plt,mapv_plt,layout=grid(2,2), size=(1200,900),  link=:x)
+combined_map=Plots.plot(mapmd_plt,mapmv_plt,mapd_plt,mapv_plt,layout=grid(2,2), size=(1200,900),  link=:x)
 
-# savefig(combined_map,"/bench/PhD/Thesis/images/cmz/a10dvMAP.png")
-
-# ###KDE FIG
-# kdes=posterior_kde(em,bivar=[1=>2,3=>4])
-
-# balx=10.:1.:144.
-# baly=[2^(24/ct)-1 for ct in balx]
-
-# ph1marg=contour(kdes[6].x,kdes[6].y,transpose(kdes[6].density), c=:viridis, xlabel="Phase 1 CT (hr)", ylabel="Phase 1 ϵ rate", colorbar=:false, xlims=[0,144], ylims=[0,5], levels=200, clim=(0,.5), legend=(.8,.3), xticks=24.:24.:144.)
-# plot!(balx,baly,color=:black,linestyle=:dash,label="Balanced pop.")
-# scatter!([mapm.θ[1]],[mapm.θ[2]], marker=:+, markersize=15, markerstrokestyle=:bold, color=:magenta, label="MAP")
-# for i in 1:5
-#     scatter!([mapm.θ[1]],[mapm.θ[2]], marker=:+, markersize=15, markerstrokestyle=:bold, color=:magenta, label=:none, framestyle=:origin)
-# end
-# lens!([15, 45], [.35, 1.6], inset = (1, bbox(0.2, 0.05, 0.75, 0.55)), colorbar=:false, framestyle=:box, subplot=2, xformatter=_->"", yformatter=_->"")
-
-# annotate!([(4,4.5,Plots.text("A",18))])
-
-# ph1ctmarg=plot(Uniform(10,144), color=:darkmagenta, fill=true, 
-# fillalpha=.5,xlims=[0,144], ylabel="p",xaxis=false, xformatter=_->"", legend=:none)
-# plot!(kdes[1].x,kdes[1].density,color=:green, fill=true, fillalpha=.5)
-# plot!([mapm.θ[1],mapm.θ[1]],[0,maximum(kdes[1].density)],color=:black,linewidth=1)
-# yflip!(ph1ctmarg)
-
-# ymarg=map(x->pdf(cycle_prior[2],x),kdes[2].x)
-# ph1ermarg=plot(ymarg,kdes[2].x, color=:darkmagenta, fill=true, fillalpha=.5, ylims=[0,5], legend=:none, yaxis=false, yformatter=_->"", xlabel="p")
-# plot!(kdes[2].density,kdes[2].x,color=:green, fill=true, fillalpha=.5)
-# plot!([0,maximum(kdes[2].density)],[mapm.θ[2],mapm.θ[2]],color=:black,linewidth=1)
-# xflip!(ph1ermarg)
-
-# marglayout=@layout [ymarg{.1w} bivar{.8h}; empty xmarg]
-
-# cbar=heatmap(transpose(ones(101,1).*(0:0.005:.5)), c=:viridis, legend=:none, yticks=:none, xticks=(1:20:101, string.(0:0.1:.5)),xlabel="Probability density")
-
-# empty=plot([],fill=true,xaxis=false,yaxis=false,grid=false, color=:darkmagenta, xformatter=_->"", yformatter=_->"", label="Prior")
-# plot!([],color=:green,fill=true,label="Posterior")
-# plot!([], color=:black, linewidth=2, label="MAP")
-# ph1c=plot(ph1ermarg,ph1marg,empty,ph1ctmarg,layout=marglayout, link=:both)
-
-# ph2marg=contour(kdes[7].x,kdes[7].y,transpose(kdes[7].density), c=:viridis, xlabel="Phase 2 CT (hr)", ylabel="Phase 2 ϵ rate", colorbar=:false, xlims=[0,144], ylims=[0,5], levels=200, clim=(0,.5), legend=(.8,.3), xticks=24.:24.:144.)
-# plot!(balx,baly,color=:black,linestyle=:dash,label="Balanced pop.")
-# scatter!([mapm.θ[3]],[mapm.θ[4]], marker=:+, markersize=15, color=:magenta, label="MAP")
-# for i in 1:5
-#     scatter!([mapm.θ[3]],[mapm.θ[4]], marker=:+, markersize=15, color=:magenta, label=:none, framestyle=:origin)
-# end
-# lens!([15, 45], [.35, 1.6], inset = (1, bbox(0.2, 0.05, 0.75, 0.55)), colorbar=:false, framestyle=:box, subplot=2, xformatter=_->"", yformatter=_->"")
-
-# annotate!(ph2marg,[(4,4.5,Plots.text("B",18))])
-
-
-# ph2ctmarg=plot(Uniform(10,144), color=:darkmagenta, fill=true, 
-# fillalpha=.5, ylabel="p", xlims=[0,144], xaxis=false, xformatter=_->"", legend=:none)
-# plot!(kdes[3].x,kdes[3].density,color=:green, fill=true, fillalpha=.5)
-# plot!([mapm.θ[3],mapm.θ[3]],[0,maximum(kdes[3].density)],color=:black,linewidth=1)
-# yflip!(ph2ctmarg)
-
-# ymarg=map(x->pdf(cycle_prior[2],x),kdes[4].x)
-# ph2ermarg=plot(ymarg,kdes[4].x, color=:darkmagenta, fill=true, fillalpha=.5, ylims=[0,5], legend=:none, yaxis=false, yformatter=_->"", xlabel="p")
-# plot!(kdes[4].density,kdes[4].x,color=:green, fill=true, fillalpha=.5)
-# plot!([0,maximum(kdes[4].density)],[mapm.θ[4],mapm.θ[4]],color=:black,linewidth=1)
-# xflip!(ph2ermarg)
-
-# marglayout2=@layout [ymarg{.1w} bivar{.8h}; empty xmarg]
-
-# ph2c=plot(ph2ermarg,ph2marg,deepcopy(empty),ph2ctmarg,layout=marglayout2, link=:both)
-
-# pend=plot(end_prior[1], color=:darkmagenta, fill=true, fillalpha=.5, xlims=[0,90], ylabel="Density", xlabel="Phase transition age", xticks=X,  label="Prior", legend=:none)
-# plot!(kdes[5].x, kdes[5].density, color=:green, fill=true, fillalpha=.5, label="Posterior")
-# plot!([mapm.θ[5],mapm.θ[5]],[0,maximum(kdes[5].density)],color=:black, label="MAP")
-# annotate!([(1,.015,Plots.text("C",18))])
-
-# combined_layout=@layout[ph1{.45h}
-#                         cbar{.01h}
-#                         ph2{.45h}
-#                         pend{.09h}]
-
-# combined_marg=plot(ph1c,cbar,ph2c,pend,layout=combined_layout,size=(1000,1200))
-
-# savefig(combined_marg,"/bench/PhD/Thesis/images/cmz/a10dvmarginals.png")
+savefig(combined_map,"/bench/PhD/Thesis/images/cmz/a10dvMAP.png")
